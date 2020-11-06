597259de9274f9831e2defb38138f80d7d64c19e
Javier <javivasv@hotmail.com>
2020-11-06 13:48:18 -0400

    Assignment 1 finished

--- assignment1-solution/app.js
+++ assignment1-solution/app.js
@@ -7,6 +7,37 @@
     LunchCheckController.$inject = ["$scope"]
     function LunchCheckController ($scope) {
         $scope.menu = ""
+        $scope.message = ""
+
+        $scope.CheckMenu = function () {
+
+            if ($scope.menu == "") {
+                $scope.message = "Please enter data first"
+            }
+
+            else {
+                var items = $scope.menu.split(",")
+
+                function CheckEmpty (noEmpty) {
+                    return noEmpty != ""
+                }
+
+                items = items.filter(CheckEmpty)
+                
+                if (items.length == 0) {
+                    $scope.message = "Please enter data first"
+
+                }
+
+                else if (items.length <= 3) {
+                    $scope.message = "Enjoy!"
+                }
+
+                else {
+                    $scope.message = "Too much!"
+                }
+            }
+        }
     }
 
 })();
